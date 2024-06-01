84637e0e300b8fe3c9125389ba7191dc3dd76c4b
luisf <luisf@Dell_Laptop>
2024-06-01 14:14:27 -0700

    6/1

--- src/Demo5/practice5.java
+++ src/Demo5/practice5.java
@@ -4,5 +4,6 @@ public class practice5 {
 	public static void main(String[] args) {
 		System.out.println("test...");
 		System.out.println("GetHub Lecture 5...");
+		System.out.println("GetHub Lecture 5...");
 	}
 }
