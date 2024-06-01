d6e2e8823636ea536b68bb3fce49050f59709427
Luis <Luis@DESKTOP-TSUKEHB>
2024-06-01 14:19:25 -0700

    6/1

--- src/Demo5/practice5.java
+++ src/Demo5/practice5.java
@@ -4,5 +4,7 @@ public class practice5 {
 	public static void main(String[] args) {
 		System.out.println("test...");
 		System.out.println("GetHub Lecture 5...");
+		System.out.println("GetHub Lecture 5...");
+		System.out.println("GetHub Lecture 5...");
 	}
 }
